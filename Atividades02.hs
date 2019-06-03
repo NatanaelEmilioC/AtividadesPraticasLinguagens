1661ef706cacea517cbd872747363ff1a658e69c
natanaelemilio <natanaelemilio@gmail.com>
2019-06-03 15:01:21 -0300

    atividades do dia 02/06

--- Atividades02.hs
+++ Atividades02.hs
@@ -1,23 +1,31 @@
 --Questão 01 lista 04
-andTern :: Int -> Int -> Int
-andTern a b
+andTerm :: Int -> Int -> Int
+andTerm a b
     | a == 1 = b
     | a == 2 , b /= 0 = a
     | otherwise = 0
 
-orTern :: Int -> Int -> Int
-orTern a b
+orTerm :: Int -> Int -> Int
+orTerm a b
     | a == 1 || b == 1 = 1
     | a == b = a
     | otherwise = 2
 
-notTern :: Int -> Int
-notTern b
+notTerm :: Int -> Int
+notTerm b
     | b == 2 = 2
     | otherwise = (b + 1) `mod` 2
 
-xorTern :: Int -> Int -> Int
-xorTern a b
-    | a == notTern b = 1
-    | a /= notTern b = 0
-    | otherwise = 2
+xorTerm :: Int -> Int -> Int
+xorTerm a b = andTerm (notTerm $ andTerm a b) $ orTerm a b  
+
+gcd1 :: Int -> Int -> Int
+gcd1 a b
+    | b == 0 = a
+    | otherwise = gcd1 b $ a `mod` b
+
+lcm1 :: Int -> Int -> Int
+lcm1 a b = div (a*b) $ gcd1 a b
+
+cosA :: Double -> Double -> Double
+cosA a b = undefined
